03a69743404747a8f2381dca989b846531875719
shaunakv1 <shaunakv1@gmail.com>
2016-12-01 23:03:09 -0500

    added some configuration to the slider

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ $(function () {
 		
 		noUiSlider.create(document.getElementById("input"), {
 				start: 50,
+				step:1,
 				orientation: "vertical",
+				direction: 'rtl',
 				range: {
 					'min': 0,
 					'max': 100
-				}
+				},
+				pips: {
+						mode: 'values',
+						values: [20, 80],
+						density: 4
+					}
 			});
 
 });
